395e93e871c09f3d76f67584dfbd4164249b7ed2
Mannan <mohdmannanansari1@gmail.com>
2025-09-15 16:06:10 +0530

    day 5 files added

---
+++ Day5/index.js
@@ -0,0 +1,40 @@
+// console.log("Hello javascript");
+// document.writeln("Hello javascript");
+// document.writeln("Hello javascript");
+// document.writeln("Hello javascript");
+// //alert message
+
+// alert("alert message");
+// console.error("error");
+
+// input methods 
+// let a=prompt("Enter the value of a :");
+
+// console.log(a);
+// var a=20;
+// console.log(a);
+// var a=30;
+// console.log(a);
+// {
+//     var a=40;
+//     console.log(a);
+// }
+// var a=70;
+// console.log(func);
+
+
+// function func(a){
+//     console.log(a);
+// }
+// console.log(func());
+
+let a=30;
+a=650;
+console.log(func);
+function func(){
+    let a=90;
+    console.log(a);
+    return 80;
+}
+console.log(func());
+
